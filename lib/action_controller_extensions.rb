f910dea7c1d7866ac814749a3f5c61ec5ad54954
aaronquint <aaronquint@66335861-e23f-4117-887d-219a79302851>
2008-02-11 16:38:04 +0000

    [NEW] Added symbol case to flash ()
    git-svn-id: svn://svn.quirkey.com/quirkey_tools/trunk/flashdance@4 66335861-e23f-4117-887d-219a79302851

--- lib/action_controller_extensions.rb
+++ lib/action_controller_extensions.rb
@@ -7,6 +7,10 @@ class ActionController::Base
       args.first.each do |flash_key, name|
         ac_flash[flash_key] = flashdance(name)
       end
+    when Symbol
+      if args[1] && (args[1].is_a?(String) || args[1].is_a?(Symbol))
+        ac_flash[args[0]] = flashdance(args[1])
+      end
     end
   end
 end
